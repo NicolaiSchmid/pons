e23d8f47549a42e77ebb496c139b11d3684712ad
Nicolai Schmid <nicolai@schmid.uno>
2026-02-14 23:41:32 +0100

    chore: migrate from next lint to eslint cli

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ const compat = new FlatCompat({
 
 export default tseslint.config(
   {
-    ignores: [".next"],
+    ignores: [".next", "convex/_generated"],
   },
   ...compat.extends("next/core-web-vitals"),
   {
@@ -44,5 +44,5 @@ export default tseslint.config(
         projectService: true,
       },
     },
-  },
+  }
 );
