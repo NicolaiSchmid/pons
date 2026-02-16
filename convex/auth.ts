e9d0354d84814ca5f4219bc7a67bac8ffb53b406
Nicolai Schmid <nicolai@schmid.uno>
2026-02-16 12:47:05 +0100

    feat: switch auth from Google to Facebook OAuth
    - Replace Google provider with Facebook in convex/auth.ts
    - Update landing page: Facebook icon, 'Continue with Facebook' button
    - ENV vars AUTH_FACEBOOK_ID and AUTH_FACEBOOK_SECRET set on dev + prod

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,6 @@
-import Google from "@auth/core/providers/google";
+import Facebook from "@auth/core/providers/facebook";
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-	providers: [Google],
+	providers: [Facebook],
 });
