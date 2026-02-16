2bea17065b0493541edcaedb480b7200bf6a2b09
Nicolai Schmid <nicolai@schmid.uno>
2026-02-16 13:49:54 +0100

    feat: request all WhatsApp Business scopes in Facebook OAuth
    Scopes: email, public_profile, business_management,
    whatsapp_business_management, whatsapp_business_messaging

--- convex/auth.ts
+++ convex/auth.ts
@@ -2,5 +2,19 @@ import Facebook from "@auth/core/providers/facebook";
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-	providers: [Facebook],
+	providers: [
+		Facebook({
+			authorization: {
+				params: {
+					scope: [
+						"email",
+						"public_profile",
+						"business_management",
+						"whatsapp_business_management",
+						"whatsapp_business_messaging",
+					].join(","),
+				},
+			},
+		}),
+	],
 });
